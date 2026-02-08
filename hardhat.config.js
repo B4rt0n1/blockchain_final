c66e306b01795819cae46b65839fcc173dbed011
Zhahangir <muratovzahongir14@gmail.com>
2026-02-08 20:05:22 +0500

    Refactor Hardhat config for Sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,16 @@
-import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import "dotenv/config";
+import { config as hardhatConfig } from "hardhat";
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 export default {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
+      type: "http",
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    }
+  }
 };
